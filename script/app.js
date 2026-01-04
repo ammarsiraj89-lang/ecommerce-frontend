fd6300dee9e60e3843014cb7fafc893e9ab8ce6b
ammarsiraj89-lang <ammarsiraj89@gmail.com>
2026-01-04 22:04:39 +0530

    ecoomerce

--- script/app.js
+++ script/app.js
@@ -1,3 +1,8 @@
-const App=()=>{
-    console.log("E-Commerce Website Loaded");
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const nav = document.getElementById("nav");
+
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+});
