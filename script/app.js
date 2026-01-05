e00b24cee3bd19d92a4a5a8cddfbf297bb57d092
ammarsiraj89-lang <ammarsiraj89@gmail.com>
2026-01-05 09:25:54 +0530

    modify

--- script/app.js
+++ script/app.js
@@ -1,12 +1,13 @@
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav");
 
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.getElementById("hamburger");
-  const nav = document.getElementById("nav");
-
-  hamburger.addEventListener("click", () => {
-    nav.classList.toggle("show");
-  });
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("show");
 });
 
+// Auto close menu when clicking a link
+document.querySelectorAll(".nav a").forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("show");
+  });
+});
